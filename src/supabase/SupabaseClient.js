cee3e96974a9007280ef822c9b5e5100a972c6fb
valentine231 <ugwuvalentine519@gmail.com>
2025-02-14 05:52:16 +0100

    small changes

--- src/supabase/SupabaseClient.js
+++ src/supabase/SupabaseClient.js
@@ -1,9 +1,6 @@
-import { supabase } from "../supabase/SupabaseClient";
+import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
